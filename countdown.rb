d3a2008e708f32f647f1e74597edb9e505887fd4
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-11 19:07:21 +0300

    Completed assignment.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    while num > 0
+        sleep(1)
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+puts countdown(10)
+
+def countdown_with_sleep(num)
+    while num > 0
+        sleep(1)
+        puts "Num: #{num}"
+        num -= 1
+    end
 end
